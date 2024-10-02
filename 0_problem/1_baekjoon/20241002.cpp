61bead859a27324675fffefdd8caa17bbac5a1e6
[서버]박빈나 <every5116@naver.com>
2024-10-02 18:05:08 +0900

    백준 Step 1
    문제 3개 풀이 완

---
+++ 0_problem/1_baekjoon/20241002.cpp
@@ -0,0 +1,48 @@
+// Problem 11382
+// https://www.acmicpc.net/problem/11382
+//#include <iostream>
+//using namespace std;
+//
+//int main(void)
+//{
+//	long input1, input2, input3;
+//
+//	cin >> input1;
+//	cin >> input2;
+//	cin >> input3;
+//
+//	cout << input1 + input2 + input3;
+//
+//	return 0;
+//}
+
+// Problem 10171
+// https://www.acmicpc.net/problem/10171
+//#include <iostream>
+//using namespace std;
+//
+//int main(void)
+//{
+//	cout << "\\    /\\" << endl;
+//	cout << " )  ( ')" << endl;
+//	cout << "(  /  )" << endl;
+//	cout << " \\(__)|" << endl;
+//
+//	return 0;
+//}
+
+// Problem 10172
+// https://www.acmicpc.net/problem/10172
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"\`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+
+	return 0;
+}
