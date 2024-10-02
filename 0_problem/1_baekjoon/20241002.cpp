e0b614b875c577d4b687231d64a12c3ee40b0a61
[서버]박빈나 <every5116@naver.com>
2024-10-02 18:12:32 +0900

    백준 Step 2
    문제 1개 풀이 완

--- 0_problem/1_baekjoon/20241002.cpp
+++ 0_problem/1_baekjoon/20241002.cpp
@@ -33,16 +33,40 @@
 
 // Problem 10172
 // https://www.acmicpc.net/problem/10172
+//#include <iostream>
+//using namespace std;
+//
+//int main(void)
+//{
+//	cout << "|\\_/|" << endl;
+//	cout << "|q p|   /}" << endl;
+//	cout << "( 0 )\"\"\"\\" << endl;
+//	cout << "|\"^\"\`    |" << endl;
+//	cout << "||_/=\\\\__|" << endl;
+//
+//	return 0;
+//}
+
+// Problem 1330
+// https://www.acmicpc.net/problem/
 #include <iostream>
 using namespace std;
 
 int main(void)
 {
-	cout << "|\\_/|" << endl;
-	cout << "|q p|   /}" << endl;
-	cout << "( 0 )\"\"\"\\" << endl;
-	cout << "|\"^\"\`    |" << endl;
-	cout << "||_/=\\\\__|" << endl;
+	int input1, input2;
+
+	cin >> input1;
+	cin >> input2;
+
+	if (input1 < input2)
+		cout << "<";
+
+	else if (input1 > input2)
+		cout << ">";
+
+	else
+		cout << "==";
 
 	return 0;
 }
