c5fbeb2928be2d5706bd788cc95c0a40641cc25a
binna <every5116@naver.com>
2024-12-06 17:31:34 +0900

    뇌를 자극하는 알고리즘 Chapter 1
    - 분리 집합

---
+++ 1_book/Part1_DataStructure/4_tree/DisjointSet.h
@@ -0,0 +1,14 @@
+#ifndef __DISJOINT_SET_H_
+#define __DISJOINT_SET_H_
+
+typedef struct tagDisjointSet
+{
+	struct tagDisjointSet* Parent;
+	void* Data;
+}DisjointSet;
+
+void DS_UnionSet(DisjointSet* Set1, DisjointSet* Set2);
+DisjointSet* DS_FindSet(DisjointSet* Set); 
+DisjointSet* DS_MakeSet(void* NewData);
+void DS_DestroySet(DisjointSet* Set);
+#endif
