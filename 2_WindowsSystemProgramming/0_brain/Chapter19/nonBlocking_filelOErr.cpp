04d7be3c98e5d7bd5e6d7636e388ba19c4224ebe
binna <every5116@naver.com>
2025-03-11 05:25:52 +0900

    뇌를 자극하는 윈도우즈 시스템 프로그래밍 - Chapter19 예제

---
+++ 2_WindowsSystemProgramming/0_brain/Chapter19/nonBlocking_filelOErr.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
+TCHAR strData1[] = _T("Test1\n");
+TCHAR strData2[] = _T("Test2\n");
+TCHAR strData3[] = _T("Test3\n");
+
+VOID WINAPI FileIOCompletionRoutine(DWORD, DWORD, LPOVERLAPPED);
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	TCHAR fileName[] = _T("data.txt"); 
+	
+	HANDLE hFile = CreateFile(
+		fileName,
+		GENERIC_WRITE, 
+		FILE_SHARE_WRITE,
+		0,
+		CREATE_ALWAYS,
+		FILE_FLAG_OVERLAPPED, 
+		0);
+
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(_T("File creation fault!\n"));
+		return -1;
+	}
+
+	OVERLAPPED overlappedInstOne;
+	memset(&overlappedInstOne, 0, sizeof(OVERLAPPED));
+	overlappedInstOne.hEvent = (HANDLE)"First I/O";
+	WriteFileEx(hFile, strData1, sizeof(strData1), &overlappedInstOne, FileIOCompletionRoutine);
+
+	OVERLAPPED overlappedInstTwo;
+	memset(&overlappedInstTwo, 0, sizeof(OVERLAPPED));
+	overlappedInstTwo.hEvent = (HANDLE)"Second I/O";
+	WriteFileEx(hFile, strData2, sizeof(strData2), &overlappedInstTwo, FileIOCompletionRoutine);
+
+	OVERLAPPED overlappedInstThree;
+	memset(&overlappedInstThree, 0, sizeof(OVERLAPPED));
+	overlappedInstThree.hEvent = (HANDLE)"First I/O";
+	WriteFileEx(hFile, strData3, sizeof(strData3), &overlappedInstThree, FileIOCompletionRoutine);
+
+	SleepEx(INFINITE, TRUE);
+	CloseHandle(hFile);
+	return 1;
+}
+
+VOID WINAPI FileIOCompletionRoutine(
+	DWORD errorCode,
+	DWORD numOfBytesTransfered,
+	LPOVERLAPPED overlapped)
+{
+	_tprintf(_T("==========File write result==========\n"));
+	_tprintf(_T("Error Code : %u\n"), errorCode);
+	_tprintf(_T("transfered bytes len : %u\n"), numOfBytesTransfered);
+	_tprintf(_T("The other info : %u\n"), (DWORD)overlapped->hEvent);
+}
